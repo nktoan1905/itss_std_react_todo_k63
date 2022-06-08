c1f0f54ea5904db3d9b69022874ac04c6b8bd2a2
nktoan1905 <nktoan1905@gmail.com>
2022-06-08 17:39:12 +0700

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,8 @@ import useStorage from "../hooks/storage";
 import { getKey } from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-    /* テストコード 開始 */
-    { key: getKey(), text: "日本語の宿題", done: false },
-    { key: getKey(), text: "reactを勉強する", done: false },
-    { key: getKey(), text: "明日の準備をする", done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+
   const [filter, setFilter] = React.useState("ALL");
 
   const displayItems = items.filter((item) => {
